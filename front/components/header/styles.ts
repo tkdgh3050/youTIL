b20255945026259849ed75c4c43af95e7b79e0ab
gomsang <tkdgh3050@gmail.com>
2023-02-13 22:05:25 +0900

    add header component

---
+++ front/components/header/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  background-color: var(--color-primary);
+  display: flex;
+  height: 60px;
+  justify-content: space-between;
+  align-items: center;
+  font-size: var(--font-size-ml);
+  color: var(--color-white);
+  padding: 0 var(--padding-size-m);
+  & span {
+  }
+`;
