43577de662e163604abb2456c5532e8b2dc89840
gomsang <tkdgh3050@gmail.com>
2023-03-02 15:24:26 +0900

    [LandigPage] add Title

--- front/pages/LandingPage/index.tsx
+++ front/pages/LandingPage/index.tsx
@@ -1,8 +1,10 @@
 import React, { FunctionComponent } from 'react';
+import Title from '../../components/title';
 
 const LandingPage: FunctionComponent = () => {
   return (
     <>
+      <Title title={'YouTIL'} />
       <div>
         LandingPage
       </div>
