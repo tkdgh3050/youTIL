b356d2d7193fcbdd55094135e281160017317afb
gomsang <tkdgh3050@gmail.com>
2023-03-09 22:34:46 +0900

    setting production mode

--- back/app.ts
+++ back/app.ts
@@ -5,6 +5,8 @@ import dotenv from "dotenv";
 import passport from "passport";
 import session from "express-session";
 import cookieParser from "cookie-parser";
+import hpp from "hpp";
+import helmet from "helmet";
 
 import userRouter from "./routes/user";
 import noteRouter from "./routes/note";
@@ -14,19 +16,30 @@ dotenv.config();
 const app = express();
 const port = 3065;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan("combined"));
+  app.use(helmet());
+  app.use(hpp());
+  app.use(
+    cors({
+      origin: true,
+      credentials: true,
+    })
+  );
+} else {
+  app.use(morgan("dev")); //front server에서 요청했을 시 로그 남겨줌
+  app.use(
+    cors({
+      origin: true, //들어올 수 있는 요청의 url 설정. true면 *과 비슷함
+      credentials: true, //실제 배포할 때는 true 로 변경해야함
+    })
+  );
+}
+
 // 아래 두개는 front 에서 보낸 request를 request.body에 넣어주는 역할을 함
 // 그래서 다른 라우터들 보다는 위에 있어야 함
 app.use(express.json()); //json으로 넘어온 데이터를 body에 넣어줌
 app.use(express.urlencoded({ extended: true })); //form 이 submit 되면 urlencoded 방식으로 넘어오는데 이걸 body에 넣어줌
-
-app.use(
-  cors({
-    origin: true, //들어올 수 있는 요청의 url 설정. true면 *과 비슷함
-    credentials: true, //실제 배포할 때는 true 로 변경해야함
-  })
-);
-
-app.use(morgan("dev")); //front server에서 요청했을 시 로그 남겨줌
 app.use(cookieParser(process.env.SECRET_COOKIE_KEY));
 app.use(
   session({
@@ -39,12 +52,13 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 passportConfig();
+
+app.get("/", (req, res) => {
+  res.send("YouTIL back server");
+});
 app.use("/user", userRouter);
 app.use("/note", noteRouter);
 
-app.get("/", (req, res) => {
-  res.send("hello express");
-});
 app.listen(port, () => {
-  console.log(`listen ${port} ...`);
+  console.log(`listen ...`);
 });
--- back/config/dbConfig.ts
+++ back/config/dbConfig.ts
@@ -2,13 +2,27 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 
 dotenv.config();
+const env = process.env.NODE_ENV;
+export let dbConfig: mysql.PoolOptions = {};
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_DATABASE } = process.env;
-export const dbConfig: mysql.PoolOptions = {
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  port: DB_PORT ? parseInt(DB_PORT) : 3306,
-  database: DB_DATABASE,
-  connectionLimit: 10,
-  dateStrings: ["DATE"],
-};
+if (env === "production") {
+  dbConfig = {
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    port: DB_PORT ? parseInt(DB_PORT) : 3306,
+    database: DB_DATABASE,
+    connectionLimit: 10,
+    dateStrings: ["DATE"],
+  };
+} else {
+  dbConfig = {
+    host: "localhost",
+    user: DB_USER,
+    password: DB_PASSWORD,
+    port: 3306,
+    database: DB_DATABASE,
+    connectionLimit: 10,
+    dateStrings: ["DATE"],
+  };
+}
