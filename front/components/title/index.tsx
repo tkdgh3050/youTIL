89a261e51f47215a0861da84f5b3f85795dc0820
gomsang <tkdgh3050@gmail.com>
2023-03-18 20:52:02 +0900

    fix #5 - 타이틀 길이가 길어지면 ... 되도록 변경

--- front/components/title/index.tsx
+++ front/components/title/index.tsx
@@ -4,6 +4,10 @@ import styled from 'styled-components';
 const TitleH2Wrapper = styled.h2`
   margin: 0;
   position: absolute;
+  white-space: nowrap;
+  overflow: hidden;
+  width: -webkit-fill-available;
+  text-overflow: ellipsis;
 `;
 
 const Title: FunctionComponent<{ title: string }> = ({ title }) => {
